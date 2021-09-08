8eb247d612b0e9762858d78d580ee32435af8967
Luiz Gustavo <59451943+LuizGustavoMR@users.noreply.github.com>
2021-09-08 15:07:19 -0300

    Criando Classes

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
-class Cliente{
-nome;
-cpf;
 
+import{Cliente} from "./Cliente.js";
 
-}
-  
-class ContaCorrente{
-
-  agencia;
-  saldo;
-  sacar(valor){ 
-
-        if(this.saldo >= valor){
-        this.saldo -= valor ;
-
- 
-      }
-    }
-}
-
-
+import{ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Luiz";
 cliente1.cpf = 99999988888;
 
+
+const cliente2 = new Cliente();
+cliente2.nome ="Barbara";
+cliente2.cpf = 11122233344;
+
+
 const contaCorrenteLuiz = new ContaCorrente;
 
-contaCorrenteLuiz.saldo = 0;
 contaCorrenteLuiz.agencia = 1001;
+contaCorrenteLuiz.cliente = cliente1;
+contaCorrenteLuiz.depositar(500);
 
-console.log(contaCorrenteLuiz.saldo);
-contaCorrenteLuiz.saldo = 100;
-console.log(contaCorrenteLuiz.saldo);
-contaCorrenteLuiz.sacar(50);
 
-console.log(contaCorrenteLuiz.saldo);
 
-console.log(cliente1);
+const conta2 = new ContaCorrente();
+
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+
+contaCorrenteLuiz.transferir(100,conta2);
+
+console.log(conta2);
+console.log(contaCorrenteLuiz);
+
