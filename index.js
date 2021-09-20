285ef614b348bf589b4803791c1d9e9055d4dbfd
Luiz Gustavo <59451943+LuizGustavoMR@users.noreply.github.com>
2021-09-20 13:18:39 -0300

    Curso finalizado , JavaScript POO
    Finalizando curso de JavaScript POO.

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-import{Cliente} from "./Cliente.js";
-
-import{ContaCorrente} from "./ContaCorrente.js";
-
-//const cliente1 = new Cliente();
-//cliente1.nome = "Luiz";
-//cliente1.cpf = 99999988888;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
 
-//const cliente2 = new Cliente();
-//cliente2.nome ="Barbara";
-//cliente2.cpf = 11122233344;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
+const conta2 = new ContaCorrente(102, cliente2);
 
-const conta1 = new ContaCorrente();
-conta1.cliente = new Cliente();
-conta1.agencia = 102;
-conta1.cliente.nome = "Luiz Gustavo";
-conta1.cliente.cpf = 11122233344;
-conta1.depositar(500);
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
 
-
-//contaCorrenteLuiz.agencia = 1001;
-//contaCorrenteLuiz.cliente = cliente1;
-//contaCorrenteLuiz.depositar(500);
-
-
-
-const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Barbara";
-conta2.cliente.cpf = 11122233344;
-conta2.agencia = 102;
-
-conta1.transferir(100,conta2);
-
-console.log(conta2);
-console.log(conta1);
-
+console.log(contaCorrenteRicardo);
